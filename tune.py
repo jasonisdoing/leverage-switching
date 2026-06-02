f6827b02196787a9ff87838f586e06b11e6ef6c2
Jason <kimwootak@catholic.ac.kr>
2026-06-02 15:11:33 +0900

    미장은 3%, 국장은 5%까지

--- tune.py
+++ tune.py
@@ -29,8 +29,8 @@ TUNING_CONFIG: dict[str, dict] = {
         ],
     },
     "kor": {
-        "drawdown_buy_cutoff": np.arange(0.5, 3.0, 0.5),
-        "drawdown_sell_cutoff": np.arange(0.5, 3.0, 0.5),
+        "drawdown_buy_cutoff": np.arange(0.5, 5.0, 0.5),
+        "drawdown_sell_cutoff": np.arange(0.5, 5.0, 0.5),
         "defense": [
             {"ticker": "CASH", "name": "현금"},
             # {"ticker": "161510", "name": "PLUS 고배당주"},
